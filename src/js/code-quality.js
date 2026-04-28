2991d202bd6de99c15f60262a15752677e96e059
jihye1 <jihye.won1@samsung.com>
2026-04-28 16:18:28 +0900

    Update code-quality.js

--- src/js/code-quality.js
+++ src/js/code-quality.js
@@ -5,3 +5,11 @@ function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
     arr[x] = 3;
   }
 }
+
+function CWE_489_569_570_571() { // CONSTANT_CONDITION
+  const x = "Fo";
+  const y = x + "Bar";
+  if (y === "FooBar") {
+    console.log('Do something');
+  }
+}
